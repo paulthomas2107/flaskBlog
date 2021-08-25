2cbb7fd57510596f6a511ef0d21b4a73e727961b
Paul Thomas <paulthomas2107@gmail.com>
2021-08-25 17:57:19 +0100

    More

--- website/views.py
+++ website/views.py
@@ -1,5 +1,7 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 from flask_login import login_required, current_user
+from .models import Post
+from . import db
 
 views = Blueprint("views", __name__)
 
@@ -9,3 +11,20 @@ views = Blueprint("views", __name__)
 @login_required
 def home():
     return render_template("home.html", user=current_user)
+
+
+@views.route("/create-post", methods=['GET', 'POST'])
+@login_required
+def create_post():
+    if request.method == "POST":
+        text = request.form.get('text')
+
+        if not text:
+            flash('Post cannot be empty', category='error')
+        else:
+            post = Post(text=text, author=current_user.id)
+            db.session.add(post)
+            db.session.commit()
+            flash('Post created !', category='success')
+
+    return render_template("create_post.html", user=current_user)
